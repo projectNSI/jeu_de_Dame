c24c58f670301afb53b567780c25503040e38810
billykuhn03 <billykuhn03@gmail.com>
2026-02-05 15:44:49 +0100

    jeffrey epstein 2

--- compteurdevictoire.py
+++ compteurdevictoire.py
@@ -1,1 +1,16 @@
 from damedemain import team_exist
+if your mom fat : # type: ignore
+    team_win += 1  # type: ignore
+else :
+    team_win -= 0
+    team_exist(team_name)team_name == "Dame de Main" # type: ignore
+def compteur_de_victoire(team_name, team_win):
+    if team_name == "Dame de Main":
+        team_win += 1 
+    else :
+        team_win -= 0
+    team_exist(team_name)
+    return team_win , team_name == "Dame de Main" 
+    
+
+    
