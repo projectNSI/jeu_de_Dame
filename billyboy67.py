ed502d58e68e0abfbf97348b3a5a99f59a6581b9
billykuhn03 <billykuhn03@gmail.com>
2026-02-05 16:35:48 +0100

    billyboy_67_fix_fonction

---
+++ billyboy67.py
@@ -0,0 +1,10 @@
+def match (jeffrey_epstein_cityboy):
+    print("start match")
+def team1 (black):
+    print("Tu es nigger")
+def team2 (white):
+    print("Hi nigger nigger")
+if team2 (white) : 
+    print( "you win")
+else :
+    print("fuck you nigger")
